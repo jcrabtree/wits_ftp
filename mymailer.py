1f06d86b3b6bb8f79749a4459db61c676687713f
David Hume <djhume@gmail.com>
2013-05-13 13:30:51 +1200

    Added GNU Public license to header of mymailer.py

--- mymailer.py
+++ mymailer.py
@@ -1,6 +1,21 @@
 '''
-This is the MyMailer class.  
-It is used to send SMS text messages as part of the spot price alert system implemented by the Electricty Authority.
+MyMailer.py - used to send SMS text messages as part of the spot price alert system implemented by the Electricty Authority.
+    Copyright (C) 2013 David Hume, Electricty Authority, New Zealand.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 This was my first serious attempt at using and implementing classes in Python so be warned...
 
 There are essentially two parts to the spot price alert system.
